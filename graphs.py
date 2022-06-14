15809ec342cca0e7487f64252640233793e68a76
cameronneylon <cn@cameronneylon.net>
2022-06-14 11:21:32 +0800

    Initial set of graphs and code

--- graphs.py
+++ graphs.py
@@ -29,7 +29,7 @@ LARGE_LANGUAGES = [
     'pt',
     'id',
     'ru',
-    'tu',
+    'tr',
     'ja',
     'ar'
 ]
@@ -53,44 +53,63 @@ def graphs():
     # Pie All Years
     graph_data = concatenate_others(downloaded)
     fig = px.pie(graph_data, values='total', names='index')
-    # fig.show()
+    fig.write_image(GRAPH_DIR / 'pie_all_years.png')
 
     # Pie All Years Non-English
     graph_data = concatenate_others(downloaded[downloaded.code != 'en'], slices=15)
     fig = px.pie(graph_data, values='total', names='index')
-    # fig.show()
+    fig.write_image(GRAPH_DIR / 'pie_allyears_nonenglish.png')
 
     # Pie 2020
     graph_data = concatenate_others(downloaded[downloaded.published_year == 2020], slices=7)
     fig = px.pie(graph_data, values='total', names='index')
-    # fig.show()
+    fig.write_image(GRAPH_DIR / 'pie_2020.png')
 
     # Pie 2020 Non-English
     graph_data = concatenate_others(downloaded[(downloaded.code != 'en') &
                                                (downloaded.published_year == 2020)], slices=15)
     fig = px.pie(graph_data, values='total', names='index')
-    # fig.show()
+    fig.write_image(GRAPH_DIR / 'pie_2020_nonenglish.png')
 
     # Pie by Crossref Type: English
     graph_data = concatenate_others(downloaded[downloaded.code == 'en'],
                                     value_column='total',
                                     group_column='crossref_type')
     fig = px.pie(graph_data, values='total', names='index')
-    fig.show()
+    fig.write_image(GRAPH_DIR / 'pie_crtype_en.png')
 
     # Pie by Crossref Type: German
     graph_data = concatenate_others(downloaded[downloaded.code == 'de'],
                                     value_column='total',
                                     group_column='crossref_type')
     fig = px.pie(graph_data, values='total', names='index')
-    fig.show()
+    fig.write_image(GRAPH_DIR / 'pie_crtype_de.png')
 
     # Pie by Crossref Type: French
     graph_data = concatenate_others(downloaded[downloaded.code == 'fr'],
                                     value_column='total',
                                     group_column='crossref_type')
     fig = px.pie(graph_data, values='total', names='index')
-    fig.show()
+    fig.write_image(GRAPH_DIR / 'pie_crtype_fr.png')
+
+    # Sunburst of Crossref Type by Language (large languages plus english)
+    graph_data = downloaded[downloaded.code.isin(LARGE_LANGUAGES + ['en'])].groupby(['name', 'crossref_type']).sum()
+    graph_data.reset_index(inplace=True)
+    fig = px.sunburst(graph_data,
+                      path=['name', 'crossref_type'],
+                      values='total')
+    fig.write_image(GRAPH_DIR / 'sunburst_crtype_large+en.png')
+
+    # Stacked Bar of Crossref Types by Langauge (large languages plus English)
+    language_totals = downloaded[['name', 'total']].groupby('name').sum().to_dict()['total']
+    graph_data['language_total'] = graph_data.name.map(language_totals)
+    graph_data.sort_values(['language_total', 'total'], ascending=False, inplace=True)
+    graph_data['pc_crossref_type'] = graph_data.total / graph_data.language_total * 100
+    fig = px.bar(graph_data,
+                 x='name',
+                 y='pc_crossref_type',
+                 color='crossref_type')
+    fig.write_image(GRAPH_DIR / 'stackedbar_crtype_large+en.png')
 
     # Languages Line Graph 2000 - 2020 - Totals
     filtered = downloaded[downloaded.code.isin(LARGE_LANGUAGES)]
@@ -99,7 +118,7 @@ def graphs():
                   x='published_year',
                   y='total',
                   color='name')
-    #fig.show()
+    fig.write_image(GRAPH_DIR / 'line_counts_overtime_largelang.png')
 
     # Languages Line Graph 2000 - 2020 - Journal Articles Only
     filtered = downloaded[(downloaded.code.isin(LARGE_LANGUAGES)) &
@@ -109,13 +128,32 @@ def graphs():
                   x='published_year',
                   y='total',
                   color='name')
-    #fig.show()
+    fig.write_image(GRAPH_DIR / 'line_counts_overtime_journal-articles_largelang.png')
 
     # Set up percentages for each year and for all years
-    combined_years = downloaded.groupby('name').sum().reset_index()
-    for df in [downloaded, combined_years]:
+    combined_years = downloaded.groupby(['code', 'name']).sum().reset_index()
+    combined_years.sort_values('total', ascending=False, inplace=True)
+
+    by_years = downloaded.groupby(['published_year', 'code', 'name']).sum().reset_index()
+    by_years.sort_values('total', ascending=False, inplace=True)
+    for df in [by_years, combined_years]:
         for col in COUNT_COLUMNS:
-            df[f'pc_{col}'] = df[f'count_{col}'] / df.total
+            df[f'pc_{col}'] = df[f'count_{col}'] / df.total * 100
+        df['pc_doaj_apc'] = df.pc_doaj - df.pc_diamond
+        df['pc_closed'] = 100 - df.pc_oa
+
+    # OA Classes for all time, large languages
+    fig = px.bar(combined_years[combined_years.code.isin(LARGE_LANGUAGES + ['en'])],
+                 x='name',
+                 y=['pc_diamond', 'pc_doaj_apc', 'pc_hybrid', 'pc_bronze', 'pc_green_only', 'pc_closed'])
+    fig.write_image(GRAPH_DIR / 'stackedbar_oaclasses_alltime_largelang.png')
+
+    # OA Classes for 2020, large languages
+    fig = px.bar(by_years[(by_years.code.isin(LARGE_LANGUAGES + ['en'])) &
+                          (by_years.published_year == 2020)],
+                 x='name',
+                 y=['pc_diamond', 'pc_doaj_apc', 'pc_hybrid', 'pc_bronze', 'pc_green_only', 'pc_closed'])
+    fig.write_image(GRAPH_DIR / 'stackedbar_oaclasses_2020_largelang.png')
 
 
 def concatenate_others(df: pd.DataFrame,
